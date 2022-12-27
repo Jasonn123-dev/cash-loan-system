e7488fbb76a9140f8190aa26e358cbfa6ef40fb0
Jasonn123-dev <110155189+Jasonn123-dev@users.noreply.github.com>
2022-12-27 11:44:44 +0800

    first commit

---
+++ src/main/java/com/sevenseven/cashloansystem/CashLoanSystemApplication.java
@@ -0,0 +1,13 @@
+package com.sevenseven.cashloansystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashLoanSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashLoanSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sevenseven/cashloansystem/CashLoanSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sevenseven.cashloansystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashLoanSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
