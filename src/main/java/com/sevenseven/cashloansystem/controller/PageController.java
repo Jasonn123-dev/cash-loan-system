f75892629e191b88487c227734340e051fa0b264
Jasonn123-dev <110155189+Jasonn123-dev@users.noreply.github.com>
2022-12-27 22:51:50 +0800

    pages

--- src/main/java/com/sevenseven/cashloansystem/controller/PageController.java
+++ src/main/java/com/sevenseven/cashloansystem/controller/PageController.java
@@ -31,5 +31,128 @@ public class PageController {
     public String forBorrower() {
         return "forAuthenticated/page5";
     }
+    @GetMapping("/teller-list")
+    public String forTellerList() {
+        return "forAuthenticated/teller-list";
+    }
+    @GetMapping("/borrower-list")
+    public String forBorrowerList() {
+        return "forAuthenticated/borrower-list";
+    }
+    @GetMapping("/official-list")
+    public String forOfficialList() {
+        return "forAuthenticated/official-list";
+    }
+    @GetMapping("/edit-borrower")
+    public String forBorrowerEdit() {
+        return "forAuthenticated/edit-borrower";
+    }
+    @GetMapping("/create-borrower")
+    public String forBorrowerCreate() {
+        return "forAuthenticated/edit-borrower";
+    }
+    @GetMapping("/add-teller")
+    public String forAddTeller() {
+        return "forAuthenticated/add-teller";
+    }
+    @GetMapping("/edit-teller")
+    public String forEditTeller() {
+        return "forAuthenticated/edit-teller";
+    }
+    @GetMapping("/add-official")
+    public String forAddOfficial() {
+        return "forAuthenticated/add-official";
+    }
+    @GetMapping("/disable-official")
+    public String forDisableOfficial() {
+        return "forAuthenticated/disable-official";
+    }
+    @GetMapping("/current-interest-rate")
+    public String forCurrentRate() {
+        return "forAuthenticated/current-interest-rate";
+    }
+    @GetMapping("/approve-loan-request")
+    public String forApprovedLoan() {
+        return "forAuthenticated/approve-loan-request";
+    }
+    @GetMapping("/disapproved-loan-request")
+    public String forDisapprovedLoan() {
+        return "forAuthenticated/disapproved-loan-request";
+
+    }
+    @GetMapping("/view-request")
+    public String forViewRequest() {
+        return "forAuthenticated/view-request";
+
+    }
+    @GetMapping("/edit-request")
+    public String forEditRequest() {
+        return "forAuthenticated/edit-request";
+
+    }
+    @GetMapping("/delete-request")
+    public String forDeleteRequest() {
+        return "forAuthenticated/delete-request";
+
+    }
+    @GetMapping("/loan-request")
+    public String forLoanRequest() {
+        return "forAuthenticated/loan-request";
+
+    }
+    @GetMapping("/borrowers-record")
+    public String forBorrowersRecord() {
+        return "forAuthenticated/borrowers-record";
+
+    }
+    @GetMapping("/review-loan")
+    public String forReviewLoan() {
+        return "forAuthenticated/review-loan";
+
+
+    }
+    @GetMapping("/update-loan")
+    public String forUpdateLoan() {
+        return "forAuthenticated/update-loan";
+
+    }
+    @GetMapping("/submit-loan")
+    public String forSubmitLoan() {
+        return "forAuthenticated/submit-loan";
+
+    }
+    @GetMapping("/register")
+    public String forRegister() {
+        return "forAuthenticated/register";
+
+    }
+    @GetMapping("/edit-info")
+    public String forEditInfo() {
+        return "forAuthenticated/edit-info";
+
+    }
+    @GetMapping("/cancel-request")
+    public String forCancelRequest() {
+        return "forAuthenticated/cancel-request";
+
+    }
+    @GetMapping("/edit-loan-request")
+    public String forEditLoanRequest() {
+        return "forAuthenticated/edit-loan-request";
+
+    }
+    @GetMapping("/current-request")
+    public String forCurrentRequest() {
+        return "forAuthenticated/current-request";
+
+    }
+    @GetMapping("/old-request")
+    public String forOldRequest() {
+        return "forAuthenticated/old-request";
+
+    }
+
+
+
 }
 
